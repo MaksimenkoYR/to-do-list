a9dab5165dfd2cfb4acd85f31cdaeb2c8fae5a2c
Maksimenko Yaroslav <maksimenko.yr@gmail.com>
2021-04-08 00:11:53 +0200

    add taskList component

--- client/src/features/TasksList/TasksList.js
+++ client/src/features/TasksList/TasksList.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import ListItem from './ListItem'
-
-const TaskList = () => {
+import {connect} from 'react-redux'
+const TasksList = ({tasks}) => {
+    console.log(tasks)
     return (
         <div>
             <ul className='collection with-header'>
-                <ListItem></ListItem>
-                <ListItem></ListItem>
-                <ListItem></ListItem>
+                {tasks.incomplete.map(i => (
+                    <li class='collection-item'>{i.taskName}</li>
+                ))}
             </ul>
         </div>
     )
 }
-
-export default TaskList
+const mapStateToProps = state => {
+    const complete = Object.values(state.taskListReducer.complete)
+    const incomplete = Object.values(state.taskListReducer.incomplete)
+    return {tasks: {complete, incomplete}}
+}
+export default connect(mapStateToProps)(TasksList)
