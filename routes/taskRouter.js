5d45e5d8f2ca904710f636c00c2c7c18f1a670a7
Maksimenko Yaroslav <maksimenko.yr@gmail.com>
2021-06-17 18:04:45 +0200

    add geting tasks from backend

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -2,8 +2,10 @@ const {Router} = require('express')
 const TaskList = require('../models/TaskList')
 const router = Router()
 
-router.post('/', (req, res) => {
-    console.log(req.user)
+router.post('/', async (req, res) => {
+    const taskList = await TaskList.findOne({user: req.user.userId})
+
+    res.status(200).json(taskList)
 })
 router.post('/add', (req, res) => {})
 module.exports = router
